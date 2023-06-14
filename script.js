fce8a9bbb196dd06ca5569a8e0ce42cc92b0bd2d
woodley <gelinw3@gmail.com>
2023-06-14 12:34:57 -0400

    Assignment3-DOM done!

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ document.querySelectorAll('.second');
 //Select a list item with a class of third, but only the list item inside of the ol tag.
 document.querySelector('ol .third');
 //Give the section with an id of container the text "Hello!".
-document.querySelector('#container').innerHTML = '<span>Hello!</span>';
-  
+//document.querySelector('#container').innerHTML = '<span>Hello!</span>';
+//Add the class main to the div with a class of footer.
+document.querySelector('.footer').classList.add('main');
+//Remove the class main on the div with a class of footer.
+document.querySelector('.footer').classList.remove('main');
+//Create a new li element.
+const li = document.createElement('li');
+//Give the li the text "four".
+li.textContent = 'four';
+//Append the li to the ul element.
+document.querySelector('ul').appendChild(li);
+//Loop over all of the lis inside the ol tag and give them a background color of "green".
+document.querySelectorAll('ol').forEach(elem =>{
+    elem.style.backgroundColor = 'green'
+})
+//Remove the div with a class of footer.
+document.querySelector('.footer').remove()
